23253e3ab192a2eb289ef4dcb2b65746c01fcad2
Amar-Bisen <bisenamar18@gmail.com>
2024-02-22 16:28:19 +0530

    CHANGES DONE BY AMAR

--- Project/process.php
+++ Project/process.php
@@ -5,7 +5,7 @@ $address2 = $_POST['address2'];
 $address3 = $_POST['address3'];
 $address4 = $_POST['address4'];
 
-// Your PHP logic to process the addresses and calculate the shortest route
+// your PHP logic to process the addresses and calculate the shortest route
 // This could involve using Google Maps API or other routing algorithms
 // Once you have the shortest route, you can output the directions or coordinates
 ?>
