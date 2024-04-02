6b205d00fee346cf9508167a440dbfa79f24a037
Amar Bisen <bisenamar18@gmail.com>
2024-04-03 00:51:56 +0530

    form updated

---
+++ Project/Dynamic-form.js
@@ -0,0 +1,18 @@
+let formfield = document.getElementById('formfield');
+
+function add(){
+    let newField = document.createElement('input');
+    newField.setAttribute('type','text');
+    newField.setAttribute('name','location');
+    newField.setAttribute('class', 'location');
+    newField.setAttribute('placeholder', 'Enter Location');
+    formfield.appendChild(newField);
+}
+
+function remove(){
+    let input_tags = formfield.getElementsByTagName('input');
+
+    if(input_tags.length >2){
+        formfield.removeChild(input_tags[(input_tags.length)-1]);
+    }
+}
