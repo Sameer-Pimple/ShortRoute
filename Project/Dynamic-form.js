12caf5357c09f6b244d4e3e027432ce133c3f732
Sameer-Pimple <sameerpimple2002@gmail.com>
2024-04-09 19:05:37 +0530

    autocomplete update

--- Project/Dynamic-form.js
+++ Project/Dynamic-form.js
@@ -1,12 +1,15 @@
 let formfield = document.getElementById('formfield');
 
-function add(){
+function add() {
     let newField = document.createElement('input');
-    newField.setAttribute('type','text');
-    newField.setAttribute('name','location');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'location');
     newField.setAttribute('class', 'location');
     newField.setAttribute('placeholder', 'Enter Location');
     formfield.appendChild(newField);
+    
+    // Call initAutocomplete to enable autocomplete on the newly created input
+    initAutocomplete();
 }
 
 function remove(){
